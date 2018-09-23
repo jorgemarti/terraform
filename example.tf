bfcf0c80d8cb65ac09df9ba96eaf608f7c146bee
Jorge Martí <jorge@jorgemarti.com>
2018-09-23 12:17:02 +0200

    Initial commit

---
+++ example.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region     = "eu-west-1"
+}
+resource "aws_instance" "web" {
+  ami           = "ami-0bdb1d6c15a40392c"
+  instance_type = "t2.micro"
+
+tags {
+  Name = "First Web Server"
+}
+}
